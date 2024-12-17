15fb29f5093cf2c779b6c269418533808ef14738
Jussi Karkkainen <juskark@gmail.com>
2024-12-17 16:42:04 +0200

    tool use ready, still needs testing

--- app/ToolUse/GeckoDriver.hs
+++ app/ToolUse/GeckoDriver.hs
@@ -1,17 +1,24 @@
-{-# Language OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module ToolUse.GeckoDriver 
   ( createSession
   , goToURL
+  , getPageSource
+  , getScreenshot
+  , sendKeys
+  , submitElement
+  , findElement
   ) where
 
 import Network.HTTP.Client
 import Data.Aeson
-import Data.ByteString.Base64 (decode)
-import qualified Data.Text.Encoding as TE
+import Data.Aeson.Types (parseEither)
 import qualified Data.ByteString.Lazy as BL
 import qualified Data.Text as T
-
+import qualified Data.Text.Encoding as TE
+import qualified Data.ByteString.Base64 as Base64
+import Data.Aeson.Lens (key, _String)
+import Control.Lens ((^?))
 
 data SessionResponse = SessionResponse
   { sessionId :: T.Text
@@ -46,31 +53,76 @@ sendPostRequest url body = do
         { method = "POST"
         , requestBody = RequestBodyLBS body
         , requestHeaders =
-            [ ("Content-Type", "application/json")
-            ]
+            [ ("Content-Type", "application/json") ]
         }
   response <- httpLbs request manager
   return $ responseBody response
 
-goToURL :: String -> String -> IO ()
+goToURL :: T.Text -> T.Text -> IO ()
 goToURL sessionId url = do
-  let endpoint = "http://localhost:4444/session/" ++ sessionId ++ "/url"
-  let body = encode $ object ["url" .= url]
+  let endpoint = "http://localhost:4444/session/" ++ T.unpack sessionId ++ "/url"
+      body = encode $ object ["url" .= url]
   response <- sendPostRequest endpoint body
   BL.putStr response
 
-getPageSource :: T.Text -> IO (Maybe Text)
+sendGetRequest :: T.Text -> IO BL.ByteString
+sendGetRequest url = do
+  manager <- newManager defaultManagerSettings
+  request <- parseRequest (T.unpack url)
+  response <- httpLbs request manager
+  return $ responseBody response
+
+getPageSource :: T.Text -> IO (Maybe T.Text)
 getPageSource sessionId = do
-  let url = "http://localhost:4444/session/" <> sessionId <> "/source"
-  response <- sendGetRequest url
-  let html = decode response :: Maybe (Object "value" Text)
-  return html
+  let url = "http://localhost:4444/session/" ++ T.unpack sessionId ++ "/source"
+  response <- sendGetRequest (T.pack url)
+  case decode response :: Maybe Value of
+    Just (Object obj) -> 
+      case parseEither (.: "value") obj of
+        Left err -> do
+          putStrLn $ "Error extracting 'value' from JSON: " ++ err
+          return Nothing
+        Right value -> return (Just value)
+    _ -> do
+      putStrLn "Failed to decode JSON response."
+      return Nothing
 
 getScreenshot :: T.Text -> IO (Maybe BL.ByteString)
 getScreenshot sessionId = do
   let url = "http://localhost:4444/session/" <> sessionId <> "/screenshot"
   response <- sendGetRequest url
-  case decode <$> decode response :: Maybe Text of
-    Just (Right screenshot) -> return (Just screenshot)
-    _ -> return Nothing
+  case decode response :: Maybe Value of
+    Just (Object obj) -> do
+      case parseEither (.: "value") obj of
+        Left err -> do
+          putStrLn $ "Error extracting 'value' from JSON: " ++ err
+          return Nothing
+        Right base64Str -> do
+          let decoded = Base64.decode (TE.encodeUtf8 base64Str)
+          return $ either (const Nothing) (Just . BL.fromStrict) decoded
+    _ -> do
+      putStrLn "Failed to decode JSON response."
+      return Nothing
+
+sendKeys :: T.Text -> T.Text -> T.Text -> IO ()
+sendKeys sessionId elementId keys = do
+  let endpoint = "http://localhost:4444/session/" ++ T.unpack sessionId ++ "/element/" ++ T.unpack elementId ++ "/value"
+      body = encode $ object ["text" .= keys, "value" .= [keys]]
+  _ <- sendPostRequest endpoint body
+  return ()
+
+submitElement :: T.Text -> T.Text -> IO ()
+submitElement sessionId elementId = do
+  let endpoint = "http://localhost:4444/session/" ++ T.unpack sessionId ++ "/element/" ++ T.unpack elementId ++ "/submit"
+  _ <- sendPostRequest endpoint ""
+  return ()
+
+findElement :: T.Text -> T.Text -> T.Text -> IO (Maybe T.Text)
+findElement sessionId strategy selector = do
+  let endpoint = "http://localhost:4444/session/" ++ T.unpack sessionId ++ "/element"
+      body = encode $ object ["using" .= strategy, "value" .= selector]
+  response <- sendPostRequest endpoint body
+  case decode response :: Maybe Value of
+    Just obj -> return $ obj ^? key "value" . key "element-6066-11e4-a52e-4f735466cecf" . _String
+    Nothing  -> return Nothing
 
--- app/ToolUse/ToolUse.hs
+++ app/ToolUse/ToolUse.hs
@@ -5,18 +5,21 @@ module ToolUse.ToolUse
   ) where
 
 import AIClient.AIClient (Command(..))
+import qualified Data.ByteString.Lazy as BL
 import ToolUse.GeckoDriver 
+import Data.Text as T
+import Control.Concurrent (threadDelay)
 
 data SearchOutput = SearchOutput 
   { query        :: String
   , searchSrc    :: Maybe String
-  , searchImg    :: Maybe String
+  , searchImg    :: Maybe BL.ByteString
   } deriving (Show)
 
 data BrowseOutput = BrowseOutput
   { url          :: String
   , browseSrc    :: Maybe String
-  , browseImg    :: Maybe String
+  , browseImg    :: Maybe BL.ByteString
   } deriving (Show)
 
 data ToolOutput = ToolOutput
@@ -25,22 +28,44 @@ data ToolOutput = ToolOutput
   } deriving (Show)
 
 search :: String -> IO SearchOutput
-search query = undefined
+search query = do
+  sessionIdM <- createSession
+  case sessionIdM of
+    Nothing -> error "Unable to create browser session" 
+    Just sessionId -> do
+      goToURL sessionId "https://www.google.com"
+      elementIdM <- findElement sessionId "name" "q"
+      case elementIdM of
+        Nothing -> error "Search bar not found"
+        Just elementId -> do
+          sendKeys sessionId elementId (T.pack query)
+          submitElement sessionId elementId
+          threadDelay 5000000 -- TODO: Find a better way to wait for search results
+          pageSrc <- getPageSource sessionId
+          screenshot <- getScreenshot sessionId
+          return $ SearchOutput query (T.unpack <$> pageSrc) screenshot
 
 browse :: String -> IO BrowseOutput
 browse url = do
   session_id <- createSession
-  goToURL session_id url
-  pageSrc <- getPageSource session_id
-  screenshot <- getScreenshot session_id
-  return $ BrowseOutput url pageSrc screenshot
+  case session_id of 
+    Just sid -> do
+      goToURL sid (T.pack url)
+      pageSrc <- getPageSource sid
+      screenshot <- getScreenshot sid
+      return $ BrowseOutput url (T.unpack <$> pageSrc) screenshot
+    Nothing -> error "Unable to start browser session"
 
-runCommand :: Command -> ToolOutput
+runCommand :: Command -> IO ToolOutput
 runCommand cmd = 
   case action cmd of
-    "search" -> ToolOutput (Just $ search (arg cmd)) Nothing
-    "browse" -> ToolOutput Nothing (Just $ browse (arg cmd))
-    _        -> ToolOutput Nothing Nothing
+    "search" -> do
+      searchRes <- search (arg cmd)
+      return $ ToolOutput (Just searchRes) Nothing
+    "browse" -> do
+      browseRes <- browse (arg cmd)
+      return $ ToolOutput Nothing (Just browseRes)
+    _        -> return $ ToolOutput Nothing Nothing
 
 executeToolUse :: [Command] -> IO [ToolOutput]
-executeToolUse cmds = return $ map runCommand cmds
+executeToolUse cmds = mapM runCommand cmds
